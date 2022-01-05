d9984f13027a95cdb3cb38738d543adfd0a9b083
Enza <86748625+Enza-von-metaTronics@users.noreply.github.com>
2022-01-05 13:52:01 +0100

    baustelle

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+function calculate() {
+    let kg = document.getElementById('kgOutput').value;
+    let height = document.getElementById('heightOutput').value;
+  
+    document.getElementById('ergebnis').value = (kg / height / height).toFixed(2);
+    if (height == '' || kg == '') {
+      alert('Deine Maßangaben hier eintippen');
+    } else {
+    }
+    document.getElementById("kgOutput").value = '';
+    document.getElementById("heightOutput").value = '';
+    
+    return kg / height / height;
+  }
