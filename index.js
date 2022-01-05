47a2ab5a24fc2f941c5bb6fca801b6046ad1b333
slavablack <veaceslav.ciornii@magenta.de>
2022-01-05 14:17:02 +0100

    lösung Veaceslav

--- index.js
+++ index.js
@@ -9,4 +9,31 @@ function calculate () {
     } else {
         
     }
+     let bmiResult = gewicht/height/height;
+    if (bmiResult <18.5) {
+         document.getElementById("textResult").innerHTML= "Untergewicht";
+         document.getElementById("textResult").style.color="green";
+     }
+     if (bmiResult>=18.5 && bmiResult <= 24.9) {
+        document.getElementById("textResult").innerHTML= "Normalgewicht";
+        document.getElementById("textResult").style.color="green";
+     }
+
+     if (bmiResult >=25 && bmiResult <= 29.9) {
+        document.getElementById("textResult").innerHTML= "Übergewicht";
+        document.getElementById("textResult").style.color="red";
+     }
+
+     if (bmiResult>=30 && bmiResult <= 34.9) {
+        document.getElementById("textResult").innerHTML= "Adipositas Grad 1";
+        document.getElementById("textResult").style.color="red";
+     }
+     if (bmiResult>=35 && bmiResult <= 39.9) {
+        document.getElementById("textResult").innerHTML= "Adipositas Grad 2";
+        document.getElementById("textResult").style.color="red";
+     }
+     if (bmiResult >=40 ) {
+        document.getElementById("textResult").innerHTML= "Adipositas Grad 3" ;
+        document.getElementById("textResult").style.color="red";
+     }
 }
