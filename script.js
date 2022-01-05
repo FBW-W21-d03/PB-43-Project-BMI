4a0d327d0b6e4b86d6be5d45295567d5e7a212c8
Christian Heinrich <christian.heinrich@digitalcareerinstitute.org>
2022-01-05 15:20:32 +0100

    Christians BMI-Rechner

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-// HTML-Elemente
-const größe = document.getElementById("größe");
-const gewicht = document.getElementById("gewicht");
-const bmi = document.getElementById("bmi");
-const info = document.getElementById("info");
+// HTML-Elemente, die irgendwann im JavaScript ausgelesen, oder verändert werden
+
+const größe = document.getElementById("größe"); // Eingabe Größe (<input>)
+const gewicht = document.getElementById("gewicht"); // Eingabe Gewicht (<input>)
+const bmi = document.getElementById("bmi"); // Ausgabe BMI (<input>)
+const info = document.getElementById("info"); // Ausgabe Gewichtsklasse (<div>)
+
+// Funktion die BMI berechnet und Ergebnis anzeigt
 
 function calcBMI() {
   let result = gewicht.value / (größe.value * größe.value);
   bmi.value = result.toFixed(1);
 
+  // Anzeige von Gewichtsklasse mit entsprechender Hintergrundfarbe
   if (result < 18.5) {
     info.innerHTML = "Untergewicht";
     info.style.backgroundColor = "#66ccff";
