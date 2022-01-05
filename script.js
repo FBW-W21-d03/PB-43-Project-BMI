7db03a1a0bfd88688d1ebeff35d2be53668c0496
Vens <v.kolev@alfatrex.com>
2022-01-05 15:09:54 +0100

    vens color fix

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function bmiRechner(){
     let gewicht = document.getElementById("kg").value;
     let hoeche = document.getElementById("groesse").value;
+    let auswert = (gewicht / hoeche / hoeche).toFixed(2);
     
     if (gewicht == "" || hoeche == ""){
         document.getElementById("wert").innerHTML = ("Ihre Werte, bitte!")
+    } else if((gewicht / hoeche / hoeche) > 29){
+        document.getElementById("wert").innerHTML = (auswert);
+        document.getElementById("wert").style.color = "red";
     } else {
-    document.getElementById("wert").innerHTML = (gewicht / hoeche / hoeche).toFixed(2);
-}
+    document.getElementById("wert").innerHTML = auswert;
+    document.getElementById("wert").style.color = "green";
+
+    } 
+    console.log(auswert);
 }
  
 
