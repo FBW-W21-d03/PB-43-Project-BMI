f54e539c1109c07ede125a15884a2f608bcca9a0
Mohal1125 <rajwindersinghmohal@yahoo.com>
2022-01-09 16:57:37 +0100

    fertig

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ function calculation() {
     let height = document.getElementById("height").value;
     console.log(weight , height)
     document.getElementById("result").innerHTML = (weight / height / height).toFixed(2);
-    if (weight === "" || height === "") {
+    if (weight === "" || height === "" || age === "") {
         alert("Please fill the following fields")
     }else{
 
     }
+    document.getElementById("weight").value = "";
+    document.getElementById("height").value = "";
+    document.getElementById("age").value = "";
     result = weight / height / height;
 }
