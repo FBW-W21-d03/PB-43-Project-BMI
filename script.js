b51501dd65fffedb6ed444630e7fd7caa26c5d82
sham912411 <shamalhakim91@gmail.com>
2022-01-10 11:47:49 +0100

    l

---
+++ script.js
@@ -0,0 +1,7 @@
+function bmiRechner(){
+    let gewicht = document.getElementById("kg").value;
+    let hoeche = document.getElementById("groesse").value;
+    console.log(bmiRechner);
+    document.getElementById("wert").innerHTML = (gewicht / hoeche / hoeche).toFixed(2);
+   
+}
